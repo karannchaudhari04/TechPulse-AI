916e9581418c23bb9f4dbea3a6c7a5076a99dbc8
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-05-05 13:23:46 +0530

    Add UserRepository with fetch methods and aggregate queries for user-liked bites

--- backend/src/main/java/com/techbite/repository/UserRepository.java
+++ backend/src/main/java/com/techbite/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.techbite.repository;
 
 import com.techbite.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
